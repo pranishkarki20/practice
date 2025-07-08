5cde7ca3d41dcc88f67db1fd9125d2c7d64eeda9
Pranish Karki <praniskkarki20@gmail.com>
2025-07-08 08:03:55 +0545

    changes

--- practice.c
+++ practice.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+
 int main() {
-    printf("hello world");
+    char answer[100];
+    printf("What is your name? ");
+    fgets(answer, sizeof(answer), stdin);
+    // Remove trailing newline if present
+    size_t len = strlen(answer);
+    if (len > 0 && answer[len - 1] == '\n') {
+        answer[len - 1] = '\0';
+    }
+    printf("Hello, %s!\n", answer);
+    return 0;
+}
+#include <stdio.h>
+int main() {
+    string answer = get_string("What is your name? ");
+    printf("Hello, %s!\n", answer);
     return 0;
 }
