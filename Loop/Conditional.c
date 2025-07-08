41ff934135748423f4b2b4ddb9c92c7f649f9e39
Pranish Karki <praniskkarki20@gmail.com>
2025-07-08 11:30:55 +0545

    commit

--- Loop/Conditional.c
+++ Loop/Conditional.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
-    int a;
+    int day;
     printf("enter a number;\n");
-   scanf("%d",&a);
-   if (a < 18)
+   scanf("%d",& day);
+   if (day < 18) 
    {
     printf("your  age is not adult age \n");
    }
