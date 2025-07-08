e0be0b16825af7345926c77e5c0fe068b4ce7b92
Pranish Karki <praniskkarki20@gmail.com>
2025-07-08 11:48:22 +0545

    commited

--- switch.c
+++ switch.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
- int main() {
-    int day ; 
-    printf ("enter a number between 1 -7 ");
+
+int main() {
+    int day;
+
+    printf("Enter a number between 1 - 7: ");
     scanf("%d", &day);
+
     switch (day) {
-        case 1  :
-        printf("sunday\n");
-        break;
-        case 2 :
-        printf("monday\n");
-        break;
-        case 3 :
-        printf("tuesday\n");
-        break;
-        case 4 :
-        printf("wednesday\n"); 
-        break; 
-        case 5 :
-        printf("thrusday\n");
-        break;
-        case 6 :
-        printf("friday\n");
-        break;
-        case 7 :    
-        printf("saturday\n");
-        break;  
-        default :
-        printf("invalid input\n");
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2:
+            printf("Monday\n");
+            break;
+        case 3:
+            printf("Tuesday\n");
+            break;
+        case 4:
+            printf("Wednesday\n");
+            break;
+        case 5:
+            printf("Thursday\n");
+            break;
+        case 6:
+            printf("Friday\n");
+            break;
+        case 7:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Invalid input\n");
+    }
 
-        return 0;
-
- }
+    return 0;
+}
