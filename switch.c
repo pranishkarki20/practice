050e9f10524c97eb9dc6714c50859cfa03485b8d
Pranish Karki <praniskkarki20@gmail.com>
2025-07-09 16:45:49 +0545

    update

--- switch.c
+++ switch.c
@@ -2,18 +2,43 @@
 int main() {
     int num;
 
-    printf("Enter a number between  0-10: ");
+    printf("Enter a number between  1-10: ");
     scanf(" %d", &num);
 
-  if (num >=30 && num <=100){
-    printf("Congratulations! You have passed \n");
-  }
-  else if (num <= 30){
-    printf("sorry! You have failed \n");
-  }
-  else {
-    printf("Invalid input! Please enter a number between 0 and 100.\n");
-  }
-  
+    switch (num) {
+        case 1:
+            printf("odd");
+            break;
+        case 2:
+            printf("even\n");
+            break;
+        case 3:
+            printf("odd\n");
+            break;
+        case 4:
+            printf("even\n");
+            break;
+        case 5:
+            printf("odd\n");
+            break;
+        case 6 :
+            printf("even\n");
+            break;
+        case 7:
+            printf("odd\n");
+            break;
+        case 8:
+        printf("even\n");   
+            break;
+            case 9:
+            printf("odd\n");    
+            break;
+        case 10:    
+        printf("even\n");
+            break;      
+        default:
+            printf("Invalid input\n");
+    }
+
     return 0;
 }
