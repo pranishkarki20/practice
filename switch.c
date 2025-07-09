db7f174254ed462feeb661bc0bd670d268e8e68d
Pranish Karki <praniskkarki20@gmail.com>
2025-07-09 22:04:19 +0545

    ,Lines

--- switch.c
+++ switch.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 int main() {
     int x = 2;
-    if (x == 1) {
+    if (x=1){
         printf("x is 1\n");
     }  
-    else if (x == 2) {
-        printf("x is 2\n");
-    }
     else {
         printf("x is neither 1 nor 2\n");
     }
 
-    return 0;
 }
