39e901fbb1483aa6bc7c0cfd79f3d82d327d3914
Pranish Karki <praniskkarki20@gmail.com>
2025-07-09 21:40:04 +0545

    uo

--- switch.c
+++ switch.c
@@ -1,43 +1,14 @@
 #include <stdio.h>
 int main() {
-    int num;
-
-    printf("Enter a number between  1-10: ");
-    scanf(" %d", &num);
-
-    switch (num) {
-        case 1:
-            printf("odd");
-            break;
-        case 2:
-            printf("even\n");
-            break;
-        case 3:
-            printf("odd\n");
-            break;
-        case 4:
-            printf("even\n");
-            break;
-        case 5:
-            printf("odd\n");
-            break;
-        case 6 :
-            printf("even\n");
-            break;
-        case 7:
-            printf("odd\n");
-            break;
-        case 8:
-        printf("even\n");   
-            break;
-            case 9:
-            printf("odd\n");    
-            break;
-        case 10:    
-        printf("even\n");
-            break;      
-        default:
-            printf("Invalid input\n");
+    int x = 2;
+    if (x == 1) {
+        printf("x is 1\n");
+    }  
+    else if (x == 2) {
+        printf("x is 2\n");
+    }
+    else {
+        printf("x is neither 1 nor 2\n");
     }
 
     return 0;
--- switch2.c
+++ switch2.c
@@ -1,32 +1,59 @@
-//This program is of nested switch i.e  switch indside switch
+
 #include <stdio.h>
 
 int main() {
-    int mainChoice = 1;
-    int subChoice = 2;
+    int mainChoice, subChoice;
+
+    // Ask for main menu choice
+    printf("Main Menu:\n");
+    printf("1. Food Menu\n");
+    printf("2. Drinks Menu\n");
+    printf("Enter your main choice (1 or 2): ");
+    scanf("%d", &mainChoice);
 
     switch (mainChoice) {
         case 1:
-            printf("Main menu: Option 1 selected\n");
+            printf("You selected: Food Menu\n");
+            printf("Sub Menu:\n");
+            printf("1. Burger\n");
+            printf("2. Pizza\n");
+            printf("Enter your food choice: ");
+            scanf("%d", &subChoice);
 
             switch (subChoice) {
                 case 1:
-                    printf("Sub menu: Option 1 selected\n");
+                    printf("You selected: Burger\n");
                     break;
                 case 2:
-                    printf("Sub menu: Option 2 selected\n");
+                    printf("You selected: Pizza\n");
                     break;
                 default:
-                    printf("Invalid sub menu choice\n");
+                    printf("Invalid food choice!\n");
             }
             break;
 
         case 2:
-            printf("Main menu: Option 2 selected\n");
+            printf("You selected: Drinks Menu\n");
+            printf("Sub Menu:\n");
+            printf("1. Coffee\n");
+            printf("2. Juice\n");
+            printf("Enter your drink choice: ");
+            scanf("%d", &subChoice);
+
+            switch (subChoice) {
+                case 1:
+                    printf("You selected: Coffee\n");
+                    break;
+                case 2:
+                    printf("You selected: Juice\n");
+                    break;
+                default:
+                    printf("Invalid drink choice!\n");
+            }
             break;
 
         default:
-            printf("Invalid main menu choice\n");
+            printf("Invalid main menu choice!\n");
     }
 
     return 0;
