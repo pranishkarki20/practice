00e39361ecbb5af24d2d6b5106dc3dbe932848f6
Pranish Karki <praniskkarki20@gmail.com>
2025-07-10 08:31:13 +0545

    updated

--- forloop.c
+++ forloop.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main() {
-    int i; //iterator , counter variable
+     int i; //iterator , counter variable
     // for loop
-    for (i=0; i<=11;i++){
+    for (i=0; i<=10;i++){
         printf(" %d", i);
     }
+    return
 }
