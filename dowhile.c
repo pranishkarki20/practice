97bc9e0978731483bd0750dd0f0a30380a640f60
Pranish Karki <praniskkarki20@gmail.com>
2025-07-11 22:36:39 +0545

    upadte

--- dowhile.c
+++ dowhile.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 int main(){
-    int s ;
-    int sum = 0;
-    printf("Enter the number of  s natural number to be printed\n");
-    scanf("%d", &s);
-    for ( int i = 1 ; i <= s; i++ ){
-       sum = sum + i;
-    }
-      printf("The sum of first %d natural numbers is %d\n", s, sum);
-    return 0;
+    int i = 1 ;
+    do {
+        printf("%d ", i);
+        i++;
+    } while (i <= 10);
 }
