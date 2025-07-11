435e7a0ca7892efacecb661b161bc13b856f3b41
Pranish Karki <praniskkarki20@gmail.com>
2025-07-11 22:34:05 +0545

    update

--- dowhile.c
+++ dowhile.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main(){
-    int i = 0 ;
-    do { 
-        i++;
-        printf("%d\n",i);
-    } while (i <= 5);
+    int s ;
+    int sum = 0;
+    printf("Enter the number of  s natural number to be printed\n");
+    scanf("%d", &s);
+    for ( int i = 1 ; i <= s; i++ ){
+       sum = sum + i;
+    }
+      printf("The sum of first %d natural numbers is %d\n", s, sum);
     return 0;
 }
