d0932f7514a129960c4b12ec33a65dd7771e49f2
Pranish Karki <praniskkarki20@gmail.com>
2025-07-11 21:47:52 +0545

    while loop addition

---
+++ whileloop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n ;
+    printf("Enter a numberm");
+    scanf ("%d",&n);
+    int i = 1;
+     while (i <= n) {
+        printf("hello world\n");
+        i++;
+     }
+}
