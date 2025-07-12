d4bd12923680463eb2432baeeded7f23e15bc76a
Pranish Karki <praniskkarki20@gmail.com>
2025-07-12 17:17:53 +0545

    Function

---
+++ function.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void printNumbers(); 
+    int main () {
+        printNumbers();
+        printf("Hello, World!\n");
+        return 0;
+    }
+    void  printNumbers() {
+        printf("1 2 3 4 5\n");
+
+    }
