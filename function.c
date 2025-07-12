dd5f780ec59d75aaacde6e81177c0a9e3fbc5385
Pranish Karki <praniskkarki20@gmail.com>
2025-07-12 20:25:46 +0545

    update

--- function.c
+++ function.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void printNumbers(); // function prototype
-    int main () {// function call
+    int main () {       // function call
         printNumbers();
         printf("Hello, World!\n");
         return 0;
     }
-    void  printNumbers() {//function definition
+    void  printNumbers() {     //function definition
         printf("1 2 3 4 5\n");
 
     }
