a75dca86c929da05c9f24b41ef8a06ecd570225e
Pranish Karki <praniskkarki20@gmail.com>
2025-07-12 16:06:46 +0545

    update

--- multipy.c
+++ multipy.c
@@ -6,7 +6,7 @@
     int product ;
      for (int i=1; i<=10; i++){
         product = a * i;
-        printf("The product of %d is %d\n", a, product);
+        printf("The product of %d * %d is %d\n", a, i, product);
      }
      return 0 ;
   }
