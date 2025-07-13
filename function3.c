bce3688cf64a6f10ef25ad1f2e5beb85fe0ca099
Pranish Karki <praniskkarki20@gmail.com>
2025-07-13 15:02:39 +0545

    commit

---
+++ function3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//function prototype 
+ int sum(int a, int b);
+
+ //function call 
+int main() {
+    printf("Enter two numbers: ");
+    int a; 
+    printf("Enter two numbers: ");
+    int b;      
+    scanf("%d %d", &a, &b);
+ int s = sum (a, b);
+    printf("The sum is: %d\n", s);  
+    return 0 ;
+}
+ int sum(int a , int b) {
+    return a + b;
+ }
