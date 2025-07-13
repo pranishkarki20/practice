dbbb357e26594ec40ee718901e5c1d2cceaca621
Pranish Karki <praniskkarki20@gmail.com>
2025-07-13 15:13:49 +0545

    update

--- function3.c
+++ function3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 //function prototype 
- int sum(int a, int b);
+  void sum(int a);
 
  //function call 
 int main() {
     printf("Enter two numbers: ");
     int a; 
-    printf("Enter two numbers: ");
-    int b;      
-    scanf("%d ", &a, &b);
- int s = sum (a, b);
-    printf("The sum is: %d\n", s);  
+   scanf("%d", &a);
+   sum (a);
     return 0 ;
 }
- int sum(int a , int b) {
-    return a + b;
+ void sum(int a) { // function definitiona
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i*a);
+    }
  }
+ 
