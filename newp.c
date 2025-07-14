113a03463747c81cb8c28d7dff47fa7763eee45c
Pranish Karki <praniskkarki20@gmail.com>
2025-07-14 12:07:17 +0545

    newp.c

---
+++ newp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ void printtable( int count ); 
+ int main()
+
+ {
+    printtable(5);
+    return 0;
+ }
+   
+ void printtable( int count )
+ {
+     if (count == 0) {
+        return;
+     }
+    printf("helloworld\n");
+    printtable(count-1);
+ }
