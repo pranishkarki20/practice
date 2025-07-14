a4ae26b7d4ac727d9c1f018060e2db28093466dc
Pranish Karki <praniskkarki20@gmail.com>
2025-07-14 13:00:49 +0545

    update

--- newp2.c
+++ newp2.c
@@ -2,13 +2,14 @@
 int sum (int n) ;
 
 int main(){
-  printf("sum of 1 to 5 is %d\n", sum(5)); // sum of 1 to 5
+  printf("Enter a number: ");
+    int n;
+    scanf("%d", &n);
+     int f = sum(n);
+  printf("sum of 1 to 5 is %d\n" , f); // sum of 1 to 5
     return 0 ;
 } 
- int sum( int n) {
-    if (n == 1) {
-    return 1 ; 
+ int sum (int n) {
+     int f = (n* 9/5) + 32; // Convert Celsius to Fahrenheit
+     return f; // Return the converted value
  }
-    int sumNm =  sum (n-1); // sum of 1 to n 
-    return sumNm * n;
-}
