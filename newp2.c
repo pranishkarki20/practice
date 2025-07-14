fe42ccde166dbcc5298b4a188bbb3c6bcba71f91
Pranish Karki <praniskkarki20@gmail.com>
2025-07-14 12:24:44 +0545

    commit

---
+++ newp2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int sum (int n) ;
+
+int main(){
+  printf("sum of 1 to 5 is %d\n", sum(5)); // sum of 1 to 5
+    return 0 ;
+} 
+ int sum( int n) {
+    if (n == 1) {
+    return 1 ; 
+ }
+    int sumNm =  sum (n-1); // sum of 1 to n 
+    return sumNm + n;
+}
